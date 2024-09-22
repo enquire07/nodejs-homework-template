1b1ec159b585c1d2eedd7c42065b46c1fb2a7948
rikki <145021641+enquire07@users.noreply.github.com>
2024-09-22 23:58:10 +0800

    hw03

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
 
-const contactsRouter = require('./routes/api/contacts')
+import { router as contactsRouter } from "./routes/api/contactsRouter.js";
 
-const app = express()
+// initialize an express application
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
 
-app.use('/api/contacts', contactsRouter)
+app.use(logger(formatsLogger));
+app.use(cors());
+// this is the JSON parser middleware
+app.use(express.json());
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+// initialize the base path for the contacts router
+app.use("/api/contacts", contactsRouter);
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+// error handling using res.status()
+// not found
+app.use((_req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
 
-module.exports = app
+// server error
+app.use((err, _req, res, _next) => {
+  res.status(500).json({ message: err.message });
+});
+
+export { app };
+
