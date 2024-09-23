d8e95cce1677fe6ccd045f00bf7b6c8aa4b95785
rikki <145021641+enquire07@users.noreply.github.com>
2024-09-24 00:24:44 +0800

    commit

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-const app = require("./app");
+import mongoose from "mongoose";
+import { app } from "./app.js";
+import dotenv from "dotenv";
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+// initialize the environment variables
+dotenv.config();
+// access the environment variables
+const { DB_HOST } = process.env; // or process.env.DB_HOST if you dont want to destructure
+
+mongoose
+  .connect(DB_HOST, {
+   
+  })
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server is running. Use our API on port: 8080");
+    });
+
+    console.log("Database connect successful");
+  })
+  .catch((error) => {
+    console.log(`Server not running. Error message: ${error.message}`);
+    process.exit(1); // this terminates the mongoose connection
+  });
+
+  
